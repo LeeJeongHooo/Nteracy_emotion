de360d9ab235df9578029d8bb8b08796e50452c4
leejeongho <jh9628771@gmail.com>
2024-01-28 22:33:03 +0900

    init: Theme 폰트 사이즈, 폰트 두께, 공간 공통화

--- emotion.d.ts
+++ emotion.d.ts
@@ -1,8 +1,16 @@
-import { ColorType } from "./src/styles/theme";
+import {
+  ColorType,
+  FontSizeType,
+  FontWeightType,
+  SpaceType,
+} from "./src/styles/theme";
 import "@emotion/react";
 
 declare module "@emotion/react" {
   export interface Theme {
     color: ColorType;
+    fontSize: FontSizeType;
+    fontWeight: FontWeightType;
+    space: SpaceType;
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,10 +9,41 @@ const color = {
   dark: "#1E1E1E",
 };
 
+const fontSize = {
+  xSmall: "1.2rem",
+  small: "1.4rem",
+  mid: "1.6rem",
+  large: "1.8rem",
+  xlarge: "2rem",
+};
+
+const fontWeight = {
+  thin: 100,
+  light: 300,
+  regular: 400,
+  mid: 500,
+  semi: 600,
+  bold: 700,
+};
+
+const space = {
+  xxSmall: "0.4rem",
+  xSmall: "0.8rem",
+  small: "1.2rem",
+  mid: "1.6rem",
+  large: "2.4rem",
+};
+
 export type ColorType = typeof color;
+export type FontSizeType = typeof fontSize;
+export type FontWeightType = typeof fontWeight;
+export type SpaceType = typeof space;
 
 const theme: Theme = {
   color,
+  fontSize,
+  fontWeight,
+  space,
 };
 
 export default theme;
