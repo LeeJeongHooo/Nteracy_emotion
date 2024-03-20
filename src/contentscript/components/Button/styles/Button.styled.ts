2a482ccb8bfd4297e0c81ae1e29b212d4ccd100b
leejeongho <jh9628771@gmail.com>
2024-03-21 03:32:07 +0900

    style: Button 스타일 수정

--- src/contentscript/components/Button/styles/Button.styled.ts
+++ src/contentscript/components/Button/styles/Button.styled.ts
@@ -4,11 +4,12 @@ export const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 100%;
   border: 0;
   border-radius: 1.2rem;
   padding: 1rem 1.2rem;
   background-color: ${({ theme, disabled }) =>
     !disabled ? theme.color.primary : theme.color.gray};
   color: ${({ theme, disabled }) => (!disabled ? "#fff" : "#000")};
-  cursor: ${({ theme, disabled }) => (!disabled ? "cursor" : "not-allowed")};
+  cursor: ${({ theme, disabled }) => (!disabled ? "pointer" : "not-allowed")};
 `;
