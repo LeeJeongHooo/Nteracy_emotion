b71bdb839381d7a70131d463979ca4db0d4e4394
leejeongho <jh9628771@gmail.com>
2024-04-02 03:29:57 +0900

    modify: 상태 변경과 마크업 변경에 따른 컴포넌트 수정

--- src/contentscript/components/RadioButton/RadioButton.tsx
+++ src/contentscript/components/RadioButton/RadioButton.tsx
@@ -1,30 +1,41 @@
 import * as S from "@src/contentscript/components/RadioButton/styles/RadioButton.styled";
-import { ChangeEvent, InputHTMLAttributes } from "react";
+import { ChangeEvent, InputHTMLAttributes, useContext, useId } from "react";
+import useRadio from "./hook/useRadio";
 
 interface IRadioButtonProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, "label"> {
   label: string;
 }
 
-const RadioButton = ({ label, name, value, ...reset }: IRadioButtonProps) => {
-  const radioId = label + name;
-  const onChange = (e: ChangeEvent) => {
-    console.log(e);
+const RadioButton = ({
+  label,
+  value,
+  disabled,
+  checked,
+  ...rest
+}: IRadioButtonProps) => {
+  const id = useId();
+  const radioId = id + label;
+
+  const { value: selectedValue, onChange } = useRadio();
+
+  const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
   };
 
+  const isChecked = selectedValue === value;
+
   return (
-    <S.RadioButton>
+    <S.RadioButton checked={isChecked} disabled={disabled}>
       <input
         id={radioId}
         type="radio"
-        name={name}
-        onChange={onChange}
+        checked={isChecked}
+        onChange={onChangeValue}
         value={value}
-        {...reset}
+        {...rest}
       />
-      <label htmlFor={radioId}>
-        <span>{label}</span>
-      </label>
+      <span>{label}</span>
     </S.RadioButton>
   );
 };
