71c41ada85c17acafc10e26927977a726d85ee2c
leejeongho <jh9628771@gmail.com>
2024-04-02 03:26:17 +0900

    style: RadioButton 마크업 및 스타일 수정

--- src/contentscript/components/RadioButton/styles/RadioButton.styled.ts
+++ src/contentscript/components/RadioButton/styles/RadioButton.styled.ts
@@ -1,13 +1,39 @@
 import styled from "@emotion/styled";
 
-export const RadioButton = styled.div`
+export const RadioButton = styled.label<{
+  checked: boolean;
+  disabled: boolean;
+}>`
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  padding: 1rem 1.2rem;
+  border-radius: 1.2rem;
+  border: 0.1rem solid ${({ theme }) => theme.color.primary};
+
+  ${({ checked, theme }) =>
+    checked &&
+    `
+    background-color: ${theme.color.primary};
+    color: #fff;
+  `}
+
+  ${({ disabled }) =>
+    disabled &&
+    `
+    opacity: 0.5;
+    cursor: not-allowed;
+  `}
+
+  &:hover {
+    color: #fff;
+
+    background-color: ${({ theme }) => theme.color.primary};
+  }
+
   input {
     display: none;
-  }
-  label {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
   }
 `;
