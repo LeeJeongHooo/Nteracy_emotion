5de0458aac70feb2d6dfa105fb1660d26bd68e67
leejeongho <jh9628771@gmail.com>
2024-04-07 11:01:20 +0900

    feat: ProgressBar 컴포넌트 생성

---
+++ src/contentscript/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,31 @@
+import * as S from "@src/contentscript/components/ProgressBar/ProgressBar.styled";
+
+// 응답 데이터 타입
+export interface IResponseType {
+  left: number;
+  right: number;
+  total: number;
+}
+
+// 응답정보가 온 뒤 보여줄 수 있는 ProgressBar
+const ProgressBar = (props: IResponseType) => {
+  const { left, right, total } = props;
+
+  const leftPercent = ((Number(left) / Number(total)) * 100).toFixed(2);
+  const rightPercent = ((Number(right) / Number(total)) * 100).toFixed(2);
+
+  return (
+    <S.ProgressBar width={leftPercent}>
+      <div>
+        <span>
+          <strong>{`${leftPercent}%`}</strong>({left}명)
+        </span>
+      </div>
+      <span>
+        <strong>{`${rightPercent}%`}</strong>({right}명)
+      </span>
+    </S.ProgressBar>
+  );
+};
+
+export default ProgressBar;
