2039f8469034937ed5a7e0b9ef8b1e206bf94c66
leejeongho <jh9628771@gmail.com>
2024-04-16 03:41:53 +0900

    style: ProgressBar 스타일 수정

--- src/contentscript/components/ProgressBar/ProgressBar.styled.ts
+++ src/contentscript/components/ProgressBar/ProgressBar.styled.ts
@@ -9,9 +9,14 @@ export const ProgressBar = styled.div<{ width: string | number }>`
   border: 0.1rem solid ${({ theme }) => theme.color.primary};
   border-radius: 1.2rem;
   > div {
+    box-sizing: border-box;
+    padding-left: ${({ theme }) => theme.space.xSmall};
     display: inline-block;
     height: 100%;
     width: ${({ width }) => `${width}%`};
     background-color: ${({ theme }) => theme.color.lightPrimary};
+  }
+  > span {
+    margin-left: ${({ theme }) => theme.space.xSmall};
   }
 `;
